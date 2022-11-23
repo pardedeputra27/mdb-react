67db108e763fd505cc43d33025054d9f82420eb0
pardedeputra27 <pardedeputra27@gmail.com>
2022-11-23 12:45:54 +0700

    first init mdb-react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import LoginPage  from "./components/LoginPage";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div>
+        <Navbar />
+        <LoginPage />
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
