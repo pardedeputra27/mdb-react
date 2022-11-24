c51d8204eba0be9763d5e02daef1b61367ae1d68
pardedeputra27 <pardedeputra27@gmail.com>
2022-11-24 10:34:12 +0700

    update page

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
-import HoverPage from "./Hover";
 import MyProfile from "./profile/MyProfile";
+import HoverPageFoto from "./profile/Hover";
 
 const GridExamplesPage = () => {
   return (
     <MDBContainer className="mb-3">
       <MDBRow>
         <MDBCol md={5}>
-          <HoverPage />
+          <HoverPageFoto />
         </MDBCol>
         <MDBCol md={7}>
             <MyProfile />
---
+++ src/components/profile/Hover.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MDBRipple } from 'mdb-react-ui-kit';
+import myFoto from '../../myFoto.png';
+
+ function HoverPageFoto() {
+  return (
+    <MDBRipple
+    className='bg-image hover-overlay shadow-1-strong rounded'
+    rippleTag='div'
+    rippleColor='light'
+  >
+    <img src={myFoto} className='w-100' />
+    <a href='#!'>
+      <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}></div>
+    </a>
+  </MDBRipple>
+  );
+}
+
+export default HoverPageFoto
