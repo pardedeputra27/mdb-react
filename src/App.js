89398cfb188e25533676a12a91ccee81530b1777
pardedeputra27 <pardedeputra27@gmail.com>
2022-11-25 10:05:12 +0700

    update version routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from "react";
-import Home from "./components/Home";
-import {BrowserRouter,Switch,Route} from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-import  NoPage from "./components/NoPage/Blank";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <React.Fragment>
-      <Navbar/>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/"><Home /></Route>
-          <Route path="/footer"><Footer/></Route>
-          <Route path="/login"><LoginPage /></Route>
-          <Route path="*"><NoPage /></Route>
-        </Switch>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
       <Footer />
-    </React.Fragment>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,61 +16,65 @@ import {
   MDBCollapse,
 } from 'mdb-react-ui-kit';
 
-const Navbar = (props) => {
+import Home from "./Home";
+import { BrowserRouter, Switch, Route,Link } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import NoPage from "./NoPage/Blank";
+import Footer from "./Footer";
+
+const Navbar = () => {
   const [showBasic, setShowBasic] = useState(false);
+  const [active, setActive] = useState(false);
+  function HandleActive() {
+    setActive(!active);
+  }
+
+  function linkActive(check){
+          if(check){
+              return 'nav-link active text-primary';
+          }else{
+            return 'nav-link';
+          }
+  }
 
   return (
-    <MDBNavbar expand='lg' light bgColor='white'>
-      <MDBContainer fluid ={false}>
-        <MDBNavbarBrand href="/">Putra</MDBNavbarBrand>
+    <BrowserRouter >
+      <MDBNavbar expand='lg' light bgColor='white'>
+        <MDBContainer fluid ={false} >
+          <Link className='navbar-brand h1' to="/">PuTRa ✌</Link>
+          <MDBNavbarToggler
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setShowBasic(!showBasic)}
+          >
+            <MDBIcon icon='bars' fas />
+          </MDBNavbarToggler>
 
-        <MDBNavbarToggler
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-          onClick={() => setShowBasic(!showBasic)}
-        >
-          <MDBIcon icon='bars' fas />
-        </MDBNavbarToggler>
+          <MDBCollapse navbar show={showBasic}>
+            <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+              <MDBNavbarItem>
+                <Link className={linkActive(active)}  to ="/" onClick={HandleActive}>Home</Link>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <Link className={linkActive(!active)}  to ="/login" onClick={HandleActive}>Login</Link>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
 
-        <MDBCollapse navbar show={showBasic}>
-          <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
-            <MDBNavbarItem>
-              <MDBNavbarLink active={showBasic} aria-current='page' href='/'>
-                Home
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href="/login">Login</MDBNavbarLink>
-            </MDBNavbarItem>
-
-            <MDBNavbarItem>
-              <MDBDropdown>
-                <MDBDropdownToggle tag='a' className='nav-link' role='button'>
-                  Dropdown
-                </MDBDropdownToggle>
-                <MDBDropdownMenu>
-                  <MDBDropdownItem link>Action</MDBDropdownItem>
-                  <MDBDropdownItem link>Another action</MDBDropdownItem>
-                  <MDBDropdownItem link>Something else here</MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavbarItem>
-
-            <MDBNavbarItem>
-              <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-                Disabled
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-          </MDBNavbarNav>
-
-          <form className='d-flex input-group w-auto'>
-            <input type='search' className='form-control' placeholder='Type query' aria-label='Search' />
-            <MDBBtn color='primary'>Search</MDBBtn>
-          </form>
-        </MDBCollapse>
-      </MDBContainer>
-    </MDBNavbar>
+            <form className='d-flex input-group w-auto'>
+              <input type='search' className='form-control' placeholder='Type query' aria-label='Search' />
+              <MDBBtn color='primary'>Search</MDBBtn>
+            </form>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/footer"><Footer /></Route>
+        <Route path="/login"><LoginPage /></Route>
+        <Route path="*"><NoPage /></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
